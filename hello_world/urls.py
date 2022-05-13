4d930b5eaf83200ff4d59a0fcd48b8640c266854
ToanPLQ <quoctoan.tn06@gmail.com>
2022-05-13 17:12:34 +0700

    clean up again

--- hello_world/urls.py
+++ hello_world/urls.py
@@ -1,5 +1,9 @@
 from turtle import home
 from django.urls import URLPattern, path
+from django.views import View
 from . import views
 
-urlpatterns=[path("",views.homepage,name="home")]
+urlpatterns=[
+    path("",views.homepage,name="home"),
+    path("sayhello",views.say_hello,name="hello")
+    ]
--- hello_world/views.py
+++ hello_world/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def homepage(request):
     return render(request,'index.html',context={})
+
+def say_hello(request):
+    return HttpResponse('Hello world')
